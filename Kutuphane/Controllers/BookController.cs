3f6e44a341dc38090409a0b4ad5de597ffd3c8c1
savasduzgun <savasdzgn@gmail.com>
2024-01-23 22:56:09 +0300

    Add project files.

---
+++ Kutuphane/Controllers/BookController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Kutuphane.Models;
+
+namespace Kutuphane.Controllers
+{
+    public class BookController : Controller
+    {
+        static List<Kitap> kitaplar = new List<Kitap>();
+        public IActionResult Kitaplar()
+        {
+            if (kitaplar.Count == 0)
+            {
+                for (int i = 1; i <= 15; i++)
+                {
+                    Kitap kitap = new Kitap
+                    {
+                        Id = i,
+                        Name = "Kitap Adı",
+                        Author = "Yazar",
+                        Publisher = "Yayın Evi",
+                        Tur = i % 2 == 0 ? Turler.Roman : Turler.Hikaye,
+                        Stock = i % 2 == 0 ? false : true,
+                    };
+                    kitaplar.Add(kitap);
+                }
+            }
+            return View(kitaplar);
+        }
+
+        //GET : /Book/KitapDetay/id
+        public IActionResult KitapDetay(int id)
+        {
+            //select top 1 * from dbo.Kitap as where k.kitaplar Id=1
+            Kitap kitap = kitaplar.FirstOrDefault(x => x.Id == id);
+
+            return View(kitap);
+        }
+
+        //GET: /Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //GET
+        //POST : Kayıt ekleme işlemleri için data post eder
+        //PUT : Güncelleme işlemleri için data post eder
+        //DELETE : Silme işlemleri için kullanılır
+        //DELETE : Silme işlemleri için kullanılır
+        //PATH : Post ve Put un birleşimi
+        [HttpPost]
+        public IActionResult Create(Kitap model)
+        {
+            kitaplar.Add(model);
+            //return View("Kitaplar", kitaplar);
+            //return RedirectToAction("Kitaplar");
+            return RedirectToAction(nameof(Kitaplar));
+        }
+        //GET : /Book/Edit/id
+        public IActionResult Edit(int id)
+        {
+            Kitap kitap = kitaplar.FirstOrDefault(k => k.Id == id);
+            return View(kitap);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Kitap model)
+        {
+            int index = kitaplar.IndexOf(kitaplar.Find(k => k.Id == model.Id));
+            kitaplar[index] = model;
+            return RedirectToAction(nameof(Kitaplar));
+        }
+        //GET : /Book/Delete/id
+        public IActionResult Delete(int id)
+        {
+            Kitap kitap = kitaplar.FirstOrDefault(k => k.Id == id);
+            return View(kitap);
+        }
+        //POST : /Book/Delete/id
+        [HttpPost]
+        public IActionResult Delete(int id, Kitap model)
+        {
+            Kitap kitap = kitaplar.FirstOrDefault(k => k.Id == id);
+            kitaplar.Remove(kitap);
+            return RedirectToAction(nameof(Kitaplar));
+        }
+    }
+}
---
+++ Kutuphane/Models/Kitap.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Kutuphane.Models
+{
+    public class Kitap
+    {
+        [DisplayName("Kitap No")]
+        public int Id { get; set; }
+        [DisplayName("Adı")]
+        public string Name { get; set; }
+        [DisplayName("Yazar")]
+        public string Author { get; set; }
+        [DisplayName("Yayın Evi")]
+        public string Publisher { get; set; }
+        [DisplayName("Tür")]
+        public Turler Tur { get; set; }
+        [DisplayName("Stok")]
+        public bool Stock { get; set; }
+    }
+
+    public enum Turler
+    {
+        Roman,
+        Hikaye,
+        Şiir
+    }
+}
